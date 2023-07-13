8ca0ce1956b4e63306a73bb0623aa437a916092c
HP <munaramamadalieva62@gmail.com>
2023-07-13 10:35:31 +0600

    commited

--- src/LMS/Decoration.java
+++ src/LMS/Decoration.java
@@ -3,4 +3,19 @@ package LMS;
 public class Decoration {
     private String local;
     private String order;
+    private String price;
+
+    public Decoration(String local,String order,String price){
+        this.local=local;
+        this.order=order;
+        this.price=price;
+    }
+
+
+
+
+
+
+
+
 }
--- src/LMS/Games.java
+++ src/LMS/Games.java
@@ -8,4 +8,10 @@ public class Games {
 
 
 
+
+
+
+
+
+
 }
